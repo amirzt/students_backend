d4232cc2f4c84b5747f52141b26cdcc447d2e1f2
unknown <gsaml7713@gmail.com>
2022-04-13 10:54:56 +0430

    migrating users and create super user

--- Users/admin.py
+++ Users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from Users.models import CustomUser, Student
+
+admin.site.register(CustomUser)
+admin.site.register(Student)
+
--- Users/models.py
+++ Users/models.py
@@ -21,18 +21,21 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
 
+    is_student = models.BooleanField(default=False)
+    is_advisor = models.BooleanField(default=False)
+
     USERNAME_FIELD = 'phone'
     REQUIRED_FIELDS = []
 
     objects = CustomUserManager()
 
-    def __str__(self):
-        return self.name
+    # def __str__(self):
+    #     return self.name
 
 
 class Student(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, primary_key=True)
-    grade = models.CharField(choices=Grades, null=False)
+    grade = models.CharField(choices=Grades, null=False, max_length=20)
     expire_date = models.DateTimeField(default=datetime.now()+timedelta(days=7))
 
 

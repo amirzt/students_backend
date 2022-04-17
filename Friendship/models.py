baf34ffea9bc45b73bd8383830a3e95ca47ab6c5
unknown <gsaml7713@gmail.com>
2022-04-17 11:32:51 +0430

    add request model

--- Friendship/models.py
+++ Friendship/models.py
@@ -3,14 +3,18 @@ from django.db import models
 
 # Create your models here.
 from Lessons.models import Lesson
-from Users.models import Grade
+from Users.models import Grade, CustomUser
 
 GENDER = [
     (1, 'پسر'),
     (2, 'دختر')
 ]
 
-
+REQUEST_STATES = [
+    (1, 'accepted'),
+    (2, 'in progress'),
+    (3, 'rejected')
+]
 class SearchAccount(models.Model):
     gender = models.CharField(choices=GENDER, max_length=4)
     grade = models.ForeignKey(Grade, on_delete=models.CASCADE)
@@ -21,3 +25,9 @@ class Subjects(models.Model):
     title = models.CharField(null=False, max_length=30)
     lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
 
+
+class Request(models.Model):
+    sender = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='sender')
+    receiver = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='receiver')
+    state = models.CharField(choices=REQUEST_STATES, max_length=11)
+    date = models.DateTimeField(auto_now_add=True)

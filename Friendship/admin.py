5e2fee92eb18f8396265833e9067c8e2d77e5a70
unknown <gsaml7713@gmail.com>
2022-04-20 09:57:11 +0430

    friendship admin

--- Friendship/admin.py
+++ Friendship/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
 # Register your models here.
+from .models import SearchAccount, Subjects, Request
+
+admin.site.register(SearchAccount)
+admin.site.register(Subjects)
+admin.site.register(Request)

9a253ad672de0f82a32b3c165312545bca6482e1
unknown <gsaml7713@gmail.com>
2022-04-24 15:42:59 +0430

    changed

--- Users/managers.py
+++ Users/managers.py
@@ -1,6 +1,6 @@
-import importlib.resources
 
-from comtypes.automation import _
+#
+# from comtypes.automation import _
 from django.contrib.auth.base_user import BaseUserManager
 
 
@@ -24,8 +24,8 @@ class CustomUserManager(BaseUserManager):
         extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('is_active', True)
 
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError(_('Superuser must have is_staff=True.'))
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError(_('Superuser must have is_superuser=True.'))
+        # if extra_fields.get('is_staff') is not True:
+        #     raise ValueError(_('Superuser must have is_staff=True.'))
+        # if extra_fields.get('is_superuser') is not True:
+        #     raise ValueError(_('Superuser must have is_superuser=True.'))
         return self.create_user(phone, password, **extra_fields)
